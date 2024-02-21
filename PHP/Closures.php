1f1b4e7aa54f4598dfd6d1432567c00e784e53bc
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-02-21 14:19:21 +0530

    php closures and folder structure

---
+++ PHP/Closures.php
@@ -0,0 +1,19 @@
+<?php
+
+$c = 10;
+
+function execute($a, $b,  Closure $fn){
+    return $fn($a, $b);
+};
+
+//to use outside variables inside a closure, use #use keyword
+
+$closure = function($a, $b) use($c){
+    return $a*$b*$c;
+};
+
+//no need in arrow functions
+$closureArrow = fn($a, $b) => $a*$b*$c;
+
+
+var_dump(execute(1,2, $closure), execute(1,2, $closureArrow));
